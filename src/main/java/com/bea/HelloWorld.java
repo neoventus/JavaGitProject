f0515095d1bf824e26ae0edec84481f6c29ec8e9
neoventus <ventus@aceboss.com>
2022-07-05 13:15:06 +0900

    test#15

--- src/main/java/com/bea/HelloWorld.java
+++ src/main/java/com/bea/HelloWorld.java
@@ -3,7 +3,7 @@ package com.bea;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("#######################################################################");
+        System.out.println("111111111111111111111111111111111111111111111111111111111111111111111111");
         System.out.println("Hello World");
         System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
     }
